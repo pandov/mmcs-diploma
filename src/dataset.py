a9c5b31aa329dd71dfe9101e48703f1c7ffe6ca8
pandov <ipandov17@gmail.com>
2021-04-27 18:46:53 +0300

    Create Transforms and Dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,63 @@
+from typing import Tuple, List, Dict
+from torch import Tensor
+from PIL.Image import Image
+
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets.folder import default_loader
 from pathlib import Path
 
-class CrackSemgentation(Dataset):
-    def __init__(self, mode: str):
-        self.files = Path(f'dataset/{mode}').rglob('*.jpg')
+class CracksDataset(Dataset):
+    def __init__(self, mode: str, transform: 'src.transforms.Compose'):
+        self.transform = transform
+        self.images = tuple(Path(f'dataset/{mode}/images').rglob('*.jpg'))
+        self.masks = tuple(Path(f'dataset/{mode}/masks').rglob('*.jpg'))
+        self._check_matches()
 
     def __len__(self):
-        return len(self.files)
+        return len(self.images)
 
-    def __getitem__(self, index: int):
-        pass
+    def _check_matches(self):
+        for image, mask in zip(self.images, self.masks):
+            assert image.name == mask.name, f'Does not match: {image.name} != {mask.name}'
+
+    def image(self, index: int) -> Image:
+        return default_loader(self.images[index])
+
+    def mask(self, index: int) -> Image:
+        return default_loader(self.masks[index])
+
+    @staticmethod
+    def is_crack_exists(masks: Tensor, threshold: float = 0.01) -> Tensor:
+        h, w = masks.shape[-2:]
+        scale = masks.sum(dim=[1, 2, 3])
+        return (scale / (h * w) >= threshold).int()
+
+    def __getitem__(self, index: int) -> Dict[str, Tensor]:
+        image = self.image(index)
+        mask = self.mask(index)
+        images, masks = self.transform(image, mask)
+        cracks = self.is_crack_exists(masks)
+        masks[cracks == 0] = torch.zeros_like(masks[cracks == 0])
+        return {
+            'images': images,
+            'masks': masks,
+            'cracks': cracks,
+        }
+
+    @staticmethod
+    def _collate_fn(batch: List[Dict[str, Tensor]]) -> Dict[str, Tensor]:
+        images, masks, cracks = zip(*((b['images'], b['masks'], b['cracks']) for b in batch))
+        return {
+            'images': torch.cat(images),
+            'masks': torch.cat(masks),
+            'cracks': torch.cat(cracks),
+        }
+
+    def get_loader(self, **kwargs) -> DataLoader:
+        return DataLoader(
+            dataset=self,
+            collate_fn=self._collate_fn,
+            pin_memory=torch.cuda.is_available(),
+            **kwargs
+        )
