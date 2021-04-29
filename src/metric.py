eb7b91ee7fe8f5e5e570c32c96121d1ffa1add93
pandov <ipandov17@gmail.com>
2021-04-29 14:25:31 +0300

    Refactoring

--- src/metric.py
+++ src/metric.py
@@ -4,20 +4,20 @@ from torch import Tensor
 import torch
 EPS = 1e-7
 
-def get_intersection_union(outputs: Tensor, targets: Tensor) -> Tuple[Tensor]:
-    intersection = (outputs * targets).sum(dim=[2, 3])
-    union = outputs.sum(dim=[2, 3]) + targets.sum(dim=[2, 3])
+def intersection_union(outputs: Tensor, targets: Tensor) -> Tuple[Tensor]:
+    intersection = (outputs * targets).sum(dim=[1, 2, 3])
+    union = outputs.sum(dim=[1, 2, 3]) + targets.sum(dim=[1, 2, 3])
     return intersection, union
 
 def dice(outputs: Tensor, targets: Tensor) -> Tensor:
-    intersection, union = get_intersection_union(outputs, targets)
+    intersection, union = intersection_union(outputs, targets)
     score = (2 * intersection) / (union + EPS)
-    return score.mean()
+    return score
 
 def iou(outputs: Tensor, targets: Tensor) -> Tensor:
-    intersection, union = get_intersection_union(outputs, targets)
+    intersection, union = intersection_union(outputs, targets)
     score = intersection / (union - intersection + EPS)
-    return score.mean()
+    return score
 
 def accuracy(outputs: Tensor, targets: Tensor, threshold: float = 0.5) -> Tensor:
     score = ((outputs > threshold).float() == targets).float()
