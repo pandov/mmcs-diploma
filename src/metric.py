699acaec9addc23c70a88845d81fd4173b42ca0c
pandov <ipandov17@gmail.com>
2021-04-30 15:45:37 +0300

    Refactoring

--- src/metric.py
+++ src/metric.py
@@ -2,23 +2,23 @@ from typing import Tuple
 from torch import Tensor
 
 import torch
+from torch.nn.functional import binary_cross_entropy as bce
 EPS = 1e-5
 
-def intersection_union(outputs: Tensor, targets: Tensor) -> Tuple[Tensor]:
-    intersection = (outputs * targets).sum(dim=[1, 2, 3])
-    union = outputs.sum(dim=[1, 2, 3]) + targets.sum(dim=[1, 2, 3])
+def intersection_union(outputs: Tensor, targets: Tensor, reduction: str = None) -> Tuple[Tensor]: # dim=[1, 2, 3]
+    intersection = (outputs * targets).sum()
+    union = outputs.sum() + targets.sum()
     return intersection, union
 
-def dice(outputs: Tensor, targets: Tensor) -> Tensor:
-    intersection, union = intersection_union(outputs, targets)
+def dice(outputs: Tensor, targets: Tensor, reduction: str = None) -> Tensor:
+    intersection, union = intersection_union(outputs, targets, reduction)
     score = (2 * intersection + EPS) / (union + EPS)
     return score
 
-def iou(outputs: Tensor, targets: Tensor) -> Tensor:
-    intersection, union = intersection_union(outputs, targets)
+def iou(outputs: Tensor, targets: Tensor, reduction: str = None) -> Tensor:
+    intersection, union = intersection_union(outputs, targets, reduction)
     score = (intersection + EPS) / (union - intersection + EPS)
     return score
 
 def accuracy(outputs: Tensor, targets: Tensor, threshold: float = 0.5) -> Tensor:
-    score = ((outputs > threshold).float() == targets).float()
-    return score.mean()
+    return ((outputs > threshold).float() == targets).float().mean()
